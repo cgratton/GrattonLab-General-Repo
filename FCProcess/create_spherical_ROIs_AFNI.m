1f7e8eef8d2fa854a94ce4d61d3f2b156691bbaa
cgratton <caterina.gratton@gmail.com>
2020-04-02 15:50:24 -0500

    edits made to corrmat maker and atlas

--- FCProcess/create_spherical_ROIs_AFNI.m
+++ FCProcess/create_spherical_ROIs_AFNI.m
@@ -4,7 +4,7 @@ function create_spherical_ROIs_AFNI
 % ADJUST the following as needed
 roiDir_top = '/projects/b1081/Atlases';
 roi_file = [roiDir_top '/Seitzman300/ROIs_300inVol_MNI_allInfo.txt']; % text file with ROI info
-tpl_out = [roiDir_top '/templateflow/tpl-MNI152NLin6Asym/tpl-MNI152NLin6Asym_res-02_desc-brain_mask_dilate3.nii.gz']; %template in correct space for output ROIs
+tpl_out = [roiDir_top '/templateflow/tpl-MNI152NLin6Asym/tpl-MNI152NLin6Asym_res-02_T1w.nii.gz']; %template in correct space for output ROIs
 orient = 'LPI'; %orientation image should be in
 outdat_fname = [roiDir_top '/Seitzman300/Seitzman300_MNI_res02_allROIs.nii.gz'];
 
@@ -30,19 +30,26 @@ for n = ROI_counter
     %expr = """'%d*step(%f-((x-%f)*(x-%f))-((y-%f)*(y-%f))-((z-%f)*(z-%f))'"""
     
     %command = sprintf('3dUndump -srad %f -master %s -orient %s -prefix %s -xyz %s',rad(n)
-    command = sprintf('module load singularity; singularity run -B %s /projects/b1081/singularity_images/afni_latest.sif 3dcalc -a %s -expr ''%d*step(%f-((x-%f)*(x-%f))-((y-%f)*(y-%f))-((z-%f)*(z-%f)))'' -LPI -prefix %s',...
-        roiDir_top,tpl_out,n,rad(n)^2,x(n),x(n),y(n),y(n),z(n),z(n),fout);
+    command = sprintf('module load singularity; singularity run -B %s /projects/b1081/singularity_images/afni_latest.sif 3dcalc -a %s -expr ''step(%f-((x-%f)*(x-%f))-((y-%f)*(y-%f))-((z-%f)*(z-%f)))'' -LPI -prefix %s',...
+        roiDir_top,tpl_out,rad(n)^2,x(n),x(n),y(n),y(n),z(n),z(n),fout);
     system(command);
     
     % load the mask that is made and sum into a final mask
-    all_mask(n,:) = load_untouch_nii_wrapper(fout);
-    
+    tmp = load_untouch_nii_wrapper(fout);
+    %tmp = logical(tmp); % putting numbers in all_mask since AFNI commands won't?
+    all_mask(n,:) = logical(tmp).* n;
+    all_mask2(n,:) = logical(tmp);
 end
 
 % then sum all of the masks together into a final mask
-avg_mask = sum(all_mask,1);
-outdat = load_untouch_nii(tpl_out);
+tot_mask = sum(all_mask,1);
+
+% remove any locations that overlap
+overlap = sum(all_mask2,1);
+tot_mask(overlap>1) = 0;
+
+outdat = load_untouch_nii(tpl_out); % note - need to be careful that this is > 8 precision or won't be able to save all ROIs
 d = size(outdat.img);
-outdat.img = reshape(avg_mask,d);
+outdat.img = reshape(tot_mask,d);
 outdat.fileprefix = outdat_fname;
 save_untouch_nii(outdat,outdat_fname);
--- FCProcess/fcimage_corrmat_volume.m
+++ FCProcess/fcimage_corrmat_volume.m
@@ -3,10 +3,13 @@ function fcimage_corrmat_volume(datafile,FCdir,atlas)
 % this function will make an ROI x ROI correlation matrix based on a set of
 % volume ROIs and a list of files
 %
+% EXAMPLE: fcimage_corrmat_volume('EXAMPLESUB_DATALIST.xlsx','/projects/b1081/iNetworks/Nifti/derivatives/preproc_FCProc/','Seitzman300')
+%
 % CG - 03.26.2020
 %%%%%%%%%%%%%%%%%
 
-%% Output directory
+%% Directory information
+atlas_dir = '/projects/b1081/Atlases/';
 outDir_top = [FCdir 'corrmats_' atlas '/'];
 if ~exist(outDir_top)
     mkdir(outDir_top);
@@ -28,28 +31,66 @@ for i = 1:numdatas
     subInfo(i).confoundsFolder = df.confoundsFolder{i}; % folder for confound inputs (assume BIDS organization)
     subInfo(i).FDtype = df.FDtype{i,1}; %use FD or fFD for tmask, etc?
     subInfo(i).runs = str2double(regexp(df.runs{i},',','split'))'; % get runs, converting to numerical array (other orientiation since that's what's expected
-    subInfo(i).space = space;
-    subInfo(i).res = res;
+    %subInfo(i).space = space; %assuming same space for now... insert check
+    %subInfo(i).res = res; %assuming same res for now.. insert check
 end
 
 %% ROI info
-
-
+atlas_params = atlas_parameters_GrattonLab(atlas,atlas_dir);
+roi_data = load_untouch_nii_wrapper(atlas_params.MNI_nii_file); %vox by 1
 
 
 %% Loop through data, extract timecourses
 for i = 1:numdatas
     
+    fprintf('Subject %s, session %d\n',subInfo(i).subjectID,subInfo(i).session);
+    
+    sess_roi_timeseries_concat{i} = [];
+    tmask_concat{i} = [];
+    
     for j = 1:length(subInfo(i).runs)
+        
+        fprintf('Run: %d\n',subInfo(i).runs(j));
         
         % FCprocessed file:
         procFile = sprintf('%s/sub-%s/ses-%d/func/sub-%s_ses-%d_task-%s_run-%02d_fmriprep_zmdt_resid_ntrpl_bpss_zmdt.nii.gz',...
             FCdir,subInfo(i).subjectID,subInfo(i).session,...
             subInfo(i).subjectID,subInfo(i).session,subInfo(i).condition,subInfo(i).runs(j));
-
+        sess_data = load_untouch_nii_wrapper(procFile); %vox by timepoints
         
+        sess_roi_timeseries{i,j} = roi_average_timecourse(sess_data,roi_data);
+        sess_roi_timeseries_concat{i} = [sess_roi_timeseries_concat{i} sess_roi_timeseries{i,j}];
+        
+        % tmask file:
+        tmaskFile = sprintf('%s/sub-%s/ses-%d/func/FD_outputs/sub-%s_ses-%d_task-%s_run-%02d_desc-tmask_%s.txt',...
+            FCdir,subInfo(i).subjectID,subInfo(i).session,...
+            subInfo(i).subjectID,subInfo(i).session,subInfo(i).condition,subInfo(i).runs(j),subInfo(i).FDtype);
+        tmask{i,j} = table2array(readtable(tmaskFile));
+        tmask_concat{i} = [tmask_concat{i}; tmask{i,j}];
+                
     end
     
+    % apply tmask to timeseries and calculate correlations
+    sess_roi_correlations{i} = paircorr_mod(sess_roi_timeseries_concat{i}(:,tmask_concat{i}));
+    
+    figure_corrmat_network_generic(sess_roi_correlatoins{i},atlas_params,[-1 1]);
+
 end
 
-aa = bb; 
+end
+
+function roi_ts_avg = roi_average_timecourse(bold_data,roi_data)
+
+nrois = unique(roi_data);
+nrois = nrois(nrois>0); % assume 0 is not an ROI
+
+for nr = 1:length(nrois)
+    roi_vox = bold_data(roi_data==nrois(nr),:);
+    roi_ts_avg(nr,:) = nanmean(roi_vox,1);
+    num_nans = sum(isnan(roi_vox(:)));
+    if num_nans>0
+        warning(sprintf('ROI %03d contains nans',nrois));
+    end
+end
+
+end
