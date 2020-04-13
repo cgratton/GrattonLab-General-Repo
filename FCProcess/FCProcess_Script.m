8d48374ba6b092f0f7780dd6fad3c8d771a44f29
btkraus <43420084+btkraus@users.noreply.github.com>
2020-04-13 15:25:57 -0500

    Added MATLAB and slurm scripts for running FC process via sbatch

---
+++ FCProcess/FCProcess_Script.m
@@ -0,0 +1,2 @@
+
+FCPROCESS_GrattonLab('/projects/b1081/member_directories/bkraus/BrainMAPD/Test_Processing/TEST_DATALIST.xlsx','/projects/b1081/member_directories/bkraus/BrainMAPD/Test_Processing/derivatives/preproc_FCProcTEST/','defaults2')
